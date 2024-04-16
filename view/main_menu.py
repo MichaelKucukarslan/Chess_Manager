93d7022274df7f0f0f42354ff1a4fb4b3189e601
Mike K <michaelkucukarslan@gmail.com>
2024-04-16 14:04:58 -0700

    Got menu to show Chess Clubs

--- view/main_menu.py
+++ view/main_menu.py
@@ -1,14 +1,21 @@
-# [ ] Get the club names
-# [ ] Display the club names
+# [x] Get the club names
+# [x] Display the club names
 # [x] display the instructions
+# [ ] Clean up class OOP
 from base_screen import BaseScreen
+import json
+from pathlib import Path
+from club import ChessClub
 
 class Main(BaseScreen):
     def __init__(self):
-        print("Enter ")
+        pass
 
     def get_command(self):
         while True:
+            print("--Main Menu--")
+            self.get_clubs()
+            print("")
             print("Type C to create a club or a club number to view/edit it.")
             print("Type T to enter Tournament Menu.")
             print("Type X to exit.")
@@ -23,6 +30,21 @@ class Main(BaseScreen):
                 pass
             elif value.upper() == "X":
                 break
+
+    def get_clubs(self):
+        data_folder="data/clubs"
+        datadir = Path(data_folder)
+        self.data_folder = datadir
+        self.clubs = []
+        for filepath in datadir.iterdir():
+            if filepath.is_file() and filepath.suffix == ".json":
+                try:
+                    self.clubs.append(ChessClub(filepath))
+                except json.JSONDecodeError:
+                    print(filepath, "is invalid JSON file.")
+
+        for index, club in enumerate (self.clubs, 1):
+            print(index , club.name)
 
     def run(self):
         self.get_command()
